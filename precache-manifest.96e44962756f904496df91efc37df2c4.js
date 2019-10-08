8b50bc1e369fd41f79c6c297e6eda6782f3864fe
suixineran <1027638598@qq.com>
2019-10-08 18:54:51 +0800

    first commit

---
+++ precache-manifest.96e44962756f904496df91efc37df2c4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b61073cc6ecc0a920579",
+    "url": "./static/js/main.b61073cc.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "./static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "a99553de9d13b9dd515c1090913b9bca",
+    "url": "./index.html"
+  }
+];
